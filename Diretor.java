89b6ab93bc1ea0b5350e03cb8a0af59e9ab7bacd
jpsoubihe <jp.soubihe@gmail.com>
2018-04-22 15:58:07 -0300

    Construtor e setBonus do salario prontos

--- Diretor.java
+++ Diretor.java
@@ -1,20 +1,35 @@
-package projeto_MC302;
 
 public class Diretor extends Pessoa{
-
+	
 	private int salario;
 	
-	public Diretor() {
-		
-		
+	public Diretor(String nome,int idade,char sexo,String dataNascimento,String RA,String senha,int salario) {
+		super(nome,idade,sexo,dataNascimento,RA,senha);
+		this.salario = salario;
 	}
-	
+
 	public int getSalario() {
 		return salario;
 	}
 
 	public void setSalario(int salario) {
 		this.salario = salario;
-	}	
+	}
 	
+	public void setBonus(Professor prof,int bonus) {
+		prof.setSalario((bonus + prof.getSalario()));
+	}
+	
+	
+	
+
+	@Override
+	public String toString() {
+		String out = super.toString();
+		out = out +"\nDiretor [salario=" + salario + "]";
+		return out;
+	}
+	
+	
+
 }
