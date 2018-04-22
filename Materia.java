c36bd391bfbb1c303bfad0dc46956bb2ab5f5310
jpsoubihe <jp.soubihe@gmail.com>
2018-04-22 15:56:23 -0300

    construtor pronto e alunosCadastrados com problema

--- Materia.java
+++ Materia.java
@@ -1,57 +1,104 @@
-package projeto_MC302;
-
 import java.util.ArrayList;
 
 public class Materia {
-
+	
 	private String nome;
+	private int codigo;
+	private int creditos;
 	private String sala;
 	private ArrayList<Aluno> alunosCadastrados;
-	private Dia dia;
 	private String horario;
 	
-	public Materia() {
+	
+	
+
+
+	public Materia(String nome, int codigo, int creditos, String sala,String horario) {
+		this.nome = nome;
+		this.codigo = codigo;
+		this.creditos = creditos;
+		this.sala = sala;
+		this.horario = horario;
+		alunosCadastrados = new ArrayList<Aluno>();
 		
 	}
+
 
 	public String getNome() {
 		return nome;
 	}
 
+
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public int getCreditos() {
+		return creditos;
+	}
+
+
+	public void setCreditos(int creditos) {
+		this.creditos = creditos;
 	}
 
 	public String getSala() {
 		return sala;
 	}
 
+
 	public void setSala(String sala) {
 		this.sala = sala;
 	}
+
 
 	public ArrayList<Aluno> getAlunosCadastrados() {
 		return alunosCadastrados;
 	}
 
+
 	public void setAlunosCadastrados(ArrayList<Aluno> alunosCadastrados) {
 		this.alunosCadastrados = alunosCadastrados;
 	}
 
-	public Dia getDia() {
-		return dia;
-	}
-
-	public void setDia(Dia dia) {
-		this.dia = dia;
-	}
 
 	public String getHorario() {
 		return horario;
 	}
 
+
 	public void setHorario(String horario) {
 		this.horario = horario;
 	}
 	
+	public boolean removerAlunosCadastrados(Aluno a) {
+		return alunosCadastrados.remove(a);
+	}
+	
+	public boolean adcionarAlunosCadastrados(Aluno a) {
+		return alunosCadastrados.add(a);
+	}
+
+
+	@Override
+	public String toString() {
+		return "Materia [nome=" + nome + ", codigo=" + codigo + ", creditos=" + creditos + ", sala=" + sala
+				+ ", alunosCadastrados=" + alunosCadastrados.toString() + ", horario=" + horario + "]";
+	}
+
+
+	
+	
+
 }
