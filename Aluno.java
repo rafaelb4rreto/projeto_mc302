0795a8cfd44a4105605c75fdc19e8dd547bd786f
jpsoubihe <jp.soubihe@gmail.com>
2018-04-22 15:57:21 -0300

    construtor e add notas e materias pronto

--- Aluno.java
+++ Aluno.java
@@ -1,8 +1,6 @@
-package projeto_MC302;
-
 import java.util.ArrayList;
 
-public class Aluno extends Pessoa{
+public class Aluno extends Pessoa {
 	
 	private ArrayList<Materia> materias;
 	private int serie;
@@ -10,59 +8,105 @@ public class Aluno extends Pessoa{
 	private int advertencias;
 	private String periodo;
 	private ArrayList<Integer> notas;
+	private static int numAlunos = 0;
 	
-	public Aluno() {
-		
-	}
-
-	public ArrayList<Materia> getMaterias() {
-		return materias;
-	}
-
-	public void setMaterias(ArrayList<Materia> materias) {
-		this.materias = materias;
-	}
-
-	public int getSerie() {
-		return serie;
-	}
-
-	public void setSerie(int serie) {
+	public Aluno(String nome,int idade,char sexo,String dataNascimento,String RA,String senha,int serie,boolean tutoria,int advertencias,String periodo) {
+		super(nome,idade,sexo,dataNascimento,RA,senha);
 		this.serie = serie;
-	}
-
-	public boolean isTutoria() {
-		return tutoria;
-	}
-
-	public void setTutoria(boolean tutoria) {
 		this.tutoria = tutoria;
-	}
-
-	public int getAdvertencias() {
-		return advertencias;
-	}
-
-	public void setAdvertencias(int advertencias) {
 		this.advertencias = advertencias;
-	}
-
-	public String getPeriodo() {
-		return periodo;
-	}
-
-	public void setPeriodo(String periodo) {
 		this.periodo = periodo;
+		materias = new ArrayList<Materia>();
+		notas = new ArrayList<Integer>();
+		numAlunos++;
 	}
-
+	
+	
+	
 	public ArrayList<Integer> getNotas() {
 		return notas;
 	}
 
+
+
 	public void setNotas(ArrayList<Integer> notas) {
 		this.notas = notas;
 	}
+
+
+
+	public ArrayList<Materia> getMaterias() {
+		return materias;
+	}
+	public void setMaterias(ArrayList<Materia> materias) {
+		this.materias = materias;
+	}
+	public int getSerie() {
+		return serie;
+	}
+	public void setSerie(int serie) {
+		this.serie = serie;
+	}
+	public boolean isTutoria() {
+		return tutoria;
+	}
+	public void setTutoria(boolean tutoria) {
+		this.tutoria = tutoria;
+	}
+	public int getAdvertencias() {
+		return advertencias;
+	}
+	public void setAdvertencias(int advertencias) {
+		this.advertencias = advertencias;
+	}
+	public String getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+
+	public boolean adicionarMateria(Materia materia) {
+		if(materias.contains(materia))
+			return false;
+		materias.add(materia);
+		notas.add(Integer.MAX_VALUE);
+		//materia.adcionarAlunosCadastrados(this);
+		return true;
+	}
 	
+	public boolean adicionarNotas(Materia materia,int nota) {
+		int a;
+		a = materias.indexOf(materia);
+		if(materias.get(a) != null) {
+			notas.set(a,nota);
+			
+			return true;
+		}
+		
+		return false;	
+	}
+	
+	public boolean removerMateria(Materia materia) {
+		if(!materias.contains(materia))
+			return false;
+		else {
+			materia.removerAlunosCadastrados(this);
+			notas.remove(materias.indexOf(materia));
+			materias.remove(materia);
+			
+			return true;
+		}
+	}
+
+
+	@Override
+	public String toString() {
+		String out = super.toString();
+		out = out + "Aluno [materias=" + materias + ", serie=" + serie + ", tutoria=" + tutoria + ", advertencias="+ advertencias + ", periodo=" + periodo + ", notas= " + notas + " numAlunos = "+numAlunos+"]";
+		return out;
+	}
+
 	
 
 }
