1baec6ece351be7cd59581de78f370f425c3a7ee
jpsoubihe <jp.soubihe@gmail.com>
2018-04-22 15:58:30 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,62 +1,63 @@
-package projeto_MC302;
 
 public class Pessoa {
+	
 	private String nome;
 	private int idade;
 	private char sexo;
 	private String dataNascimento;
-	private String RA;
+	private final String RA;
 	private String senha;
 	
-	public Pessoa() {
-		
+	
+	public Pessoa(String nome,int idade,char sexo,String dataNascimento,String RA,String senha) {
+		this.nome = nome;
+		this.idade = idade;
+		this.sexo = sexo;
+		this.dataNascimento = dataNascimento;
+		this.RA = RA;
+		this.senha = senha;
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public int getIdade() {
 		return idade;
 	}
-
 	public void setIdade(int idade) {
 		this.idade = idade;
 	}
-
 	public char getSexo() {
 		return sexo;
 	}
-
 	public void setSexo(char sexo) {
 		this.sexo = sexo;
 	}
-
 	public String getDataNascimento() {
 		return dataNascimento;
 	}
-
 	public void setDataNascimento(String dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
-
 	public String getRA() {
 		return RA;
 	}
-
-	public void setRA(String rA) {
-		RA = rA;
-	}
-
 	public String getSenha() {
 		return senha;
 	}
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + ", dataNascimento=" + dataNascimento
+				+ ", RA=" + RA + ", senha=" + senha + "]";
+	}
+	
+	
+	
 }
