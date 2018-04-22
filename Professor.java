962fbdb5e1c3eaedc0cb8e45a4d675a2ac34e012
jpsoubihe <jp.soubihe@gmail.com>
2018-04-22 15:55:21 -0300

    Construtor e tutorados implementados

--- Professor.java
+++ Professor.java
@@ -1,25 +1,28 @@
-package projeto_MC302;
-
 import java.util.ArrayList;
 
 public class Professor extends Pessoa{
-	
-	private int serie;
+
 	private ArrayList<Materia> materias;
 	private ArrayList<Aluno> tutorados;
 	private String sala;
 	private int salario;
+	private static int numMaxTutorados;
 	
-	public Professor() {
-		
+
+	public Professor(String nome,int idade,char sexo,String dataNascimento,String RA,String senha,String sala,int salario) {
+		super(nome,idade,sexo,dataNascimento,RA,senha);
+		materias = new ArrayList<Materia>();
+		tutorados = new ArrayList<Aluno>();
+		this.sala = sala;
+		this.salario = salario;
+	}
+	
+	public static int getNumMaxTutorados() {
+		return numMaxTutorados;
 	}
 
-	public int getSerie() {
-		return serie;
-	}
-
-	public void setSerie(int serie) {
-		this.serie = serie;
+	public static void setNumMaxTutorados(int numMaxTutorados) {
+		Professor.numMaxTutorados = numMaxTutorados;
 	}
 
 	public ArrayList<Materia> getMaterias() {
@@ -52,6 +55,31 @@ public class Professor extends Pessoa{
 
 	public void setSalario(int salario) {
 		this.salario = salario;
+	}
+	
+	public boolean adicionarTutorado(Aluno aluno) {
+		if(tutorados.size() < numMaxTutorados) {
+			return tutorados.add(aluno);
+		}
+		
+		return false;
+	}
+	
+	public boolean removerTutorado(Aluno aluno) {
+		if(tutorados.contains(aluno)) {
+			return tutorados.remove(aluno);
+		}
+		
+		return false;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		String out = super.toString();
+		out = out +  "\nProfessor [tutorados=" + tutorados + ", sala=" + sala + ", salario=" + salario +", materias="+ materias + "]";
+		return out;
 	}
 	
 	
