c27d9ccb31f3f7da17522132342207bd66e0c88c
gubraz <38592740+gubraz@users.noreply.github.com>
2018-04-22 22:40:52 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,20 @@
-import java.util.ArrayList;
-
-
 public class Main {
 	
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa("Maria",57,'F',"20/07/1960","0001","1a2b3c");
-		Pessoa p2 = new Pessoa("Joao",44,'M',"03/05/1974","1001","abcd");
-		Pessoa p3 = new Pessoa("Jose",12,'M',"02/06/2005","2001","1234");
+		Pessoa p1 = new Pessoa("Maria",57,'F',"20/07/1960","1a2b3c");
+		Pessoa p2 = new Pessoa("Joao",44,'M',"03/05/1974","abcd");
+		Pessoa p3 = new Pessoa("Jose",12,'M',"02/06/2005","1234");
 		
-		Diretor d = new Diretor("Maria",57,'F',"20/07/1960","0001","1a2b3c",20000);
+		Diretor d = new Diretor("Maria",57,'F',"20/07/1960","1a2b3c",20000);
 		//System.out.println(d.toString());
 		
-		Professor prof = new Professor("Joao",44,'M',"03/05/1974","1001","abcd","134B",10000);
+		Professor prof = new Professor("Joao",44,'M',"03/05/1974","abcd","134B",10000);
 		//System.out.println(prof.toString());
-		prof.setNumMaxTutorados(4);
+		Professor.setNumMaxTutorados(4);
 		
-		Aluno a1 = new Aluno("Jose",12,'M',"02/06/2005","2001","1234",5,false,0,"matutino");
-		System.out.println(a1.toString());
+		Aluno a1 = new Aluno("Jose",12,'M',"02/06/2005","1234",5,false,0,"matutino");
+		Aluno a2 = new Aluno("Clarinha",2,'F',"02/06/2015","1234",5,false,0,"matutino");
+	//	System.out.println(a1.toString());
 		
 		prof.adicionarTutorado(a1);
 		
@@ -25,20 +23,26 @@ public class Main {
 		
 		Materia m2 = new Materia("Matematica",2,5,"66B","08h");
 		
+		Materia m3 = new Materia("Calculo Avancado", 2, 4, "22A", "14h");
+		
 		a1.adicionarMateria(m1);
 		a1.adicionarMateria(m2);
-		System.out.println("\n"+a1.toString());
-		System.out.println("\n"+prof.toString());
+		a2.adicionarMateria(m3);
+		a1.adicionarMateria(m3);
+		System.out.println("\n"+a1);
+
+		System.out.println("\n"+a2);
+		
+		System.out.println("\n"+m3);
+		
+		//		System.out.println("\n"+prof);
 		
 		prof.removerTutorado(a1);
-		System.out.println("\n"+prof.toString());
+//		System.out.println("\n"+prof.toString());
 		
 		d.setBonus(prof, 1000);
-		System.out.println("\n"+prof.toString());
+//		System.out.println("\n"+prof.toString());
 		
-
-
-
 	}
 
 }
