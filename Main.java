caab8ba590f2b71a8864edd008e6c8c87e3d4d25
jpsoubihe <jp.soubihe@gmail.com>
2018-04-24 17:54:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -5,50 +7,71 @@ public class Main {
 		Pessoa p2 = new Pessoa("Joao",44,'M',"03/05/1974","abcd");
 		Pessoa p3 = new Pessoa("Jose",12,'M',"02/06/2005","1234");
 		
-		Diretor d = new Diretor("Maria",57,'F',"20/07/1960","1a2b3c",20000);
+		Aluno a3 = new Aluno("Paulo",12,'M',"02/06/2005","1234",5,false,0,0,"matutino");
+
+		Diretor d = new Diretor("Maria",57,'F',"20/07/1960","0001","1a2b3c",20000);
 		//System.out.println(d.toString());
 		
 		Professor prof = new Professor("Joao",44,'M',"03/05/1974","abcd","134B",10000);
-		Professor prof2 = new Professor("Joaquim",44,'M',"03/05/1974","abcd","134B",10000);
 		//System.out.println(prof.toString());
-		Professor.setNumMaxTutorados(4);
+		prof.setNumMaxTutorados(4);
+		Materia m1 = new Materia("Geografia","1",4,"66A","10h",Dia.SEGUNDA,30);
+		Materia m2 = new Materia("Matematica","2",5,"66B","08h",Dia.SEGUNDA,20);
+		Materia m3 = new Materia("Ciencias","3",3,"66C","10h",Dia.SEGUNDA,20);
+		Aluno a1 = new Aluno("Jose",12,'M',"02/06/2005","1234",5,false,0,0,"matutino");
 		
-		Aluno a1 = new Aluno("Jose",12,'M',"02/06/2005","1234",5,false,0,"matutino");
-		Aluno a2 = new Aluno("Clarinha",2,'F',"02/06/2015","1234",5,false,0,"matutino");
-	//	System.out.println(a1.toString());
+		d.setMaxCred(a1, 4); 
+		a1.adicionarMateria(m1);
+		a1.adicionarMateria(m2);
+		System.out.println("\n"+a1); //teste de adição de materias levando em conta numero de creditos
+		
+		d.setMaxCred(a1, 20);
+		a3.adicionarMateria(m1);
+		a3.adicionarMateria(m3);
+		System.out.println("\n"+a3); //teste de adição de materias levando em conta horario
+		
+		a3.adicionarNotas(m1, 9.5f);
+		System.out.println("\n"+a3); //teste de adição de notas
+
+		
+		a3.removerMateria(m1);
+		System.out.println("\n"+a3); //remoção de materias 
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
 		
 		prof.adicionarTutorado(a1);
 		
-		Materia m1 = new Materia("Geografia","G 001",4,"66A","10h", Dia.SEGUNDA);
-		//System.out.println(m1.toString());
 		
-		Materia m2 = new Materia("Matematica","MA002",5,"66B","08h",Dia.QUINTA);
+		//System.out.println(m1.toString());		
 		
-		Materia m3 = new Materia("Calculo Avancado","MA666", 4, "22A", "14h", Dia.SEXTA);
 		
-		prof.adicionarMateria(m3);
-		prof2.adicionarMateria(m3);
 		
-		a1.adicionarMateria(m1);
-		a1.adicionarMateria(m2);
-		a2.adicionarMateria(m3);
-		a1.adicionarMateria(m3);
-		a1.removerMateria(m3);
-		System.out.println("\n"+a1);
+		prof.adicionarMateria(m1);
+		//System.out.println("\n"+m1.toString());
 
-		System.out.println("\n"+a2);
+		//System.out.println("\n"+a1.toString());
+
+		//System.out.println("\n"+m1.toString());
 		
-		System.out.println("\n"+m3);
-		
-		
-		//		System.out.println("\n"+prof);
 		
 		prof.removerTutorado(a1);
-//		System.out.println("\n"+prof.toString());
+		
 		
 		d.setBonus(prof, 1000);
-//		System.out.println("\n"+prof.toString());
 		
+		//System.out.println("\n"+prof.toString());
+		
+
+
+
 	}
 
 }
