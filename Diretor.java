592e5f00ca97c03c117ac99c13702c4b8175590c
gubraz <38592740+gubraz@users.noreply.github.com>
2018-06-13 20:03:09 -0300

    Update Diretor.java

--- Diretor.java
+++ Diretor.java
@@ -9,6 +9,35 @@ public class Diretor extends Pessoa{
 		this.salario = salario;
 	}
 	
+	public demitirProfessor(Professor prof){
+			
+	}	
+	
+	public expulsarAluno(Aluno aluno){
+		
+	}
+	
+	public matricularAluno(Aluno aluno){
+		
+	}
+	
+	public contratarProfessor(Professor prof){
+		
+	}
+	
+	public atribuirMateriaAUmProfessor(Professor prof, Materia materia){
+	//sala	
+	}
+	
+	public abrirMateria(String nome, String ementa){
+	
+	}
+	
+	public fecharMateria(Materia materia){
+		
+	}
+	
+	
 	@Override
 	public String toString() {
 		String out = super.toString();
