353815cf060de1ab48b9395ae1572399c8cd642e
jpsoubihe <jp.soubihe@gmail.com>
2018-06-23 15:40:06 -0300

    Login de diretor e professor disponivel

--- Login.java
+++ Login.java
@@ -1,7 +1,5 @@
 import javax.swing.*;
-import java.awt.Container;
 import java.awt.FlowLayout;
-import java.awt.event.*;
 
 public class Login extends JFrame {
 	/**
@@ -9,7 +7,8 @@ public class Login extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public Login() {
+	@SuppressWarnings("deprecation")
+	public Login(Diretor d) {
 		
 		Object[] classes = {"Aluno","Professor","Diretor"};
 		int answer = JOptionPane.showOptionDialog(null,"Que área gostaria de fazer login?","Tela de inicio", JOptionPane.INFORMATION_MESSAGE, JOptionPane.INFORMATION_MESSAGE,null, classes, classes[2]);
@@ -18,51 +17,24 @@ public class Login extends JFrame {
 		if(answer == 0) { //Aluno esta tentando logar no sistema
 			//String ra = JOptionPane.showInputDialog(null, "RA : ", "Cadastro", JOptionPane.PLAIN_MESSAGE);
 			
-			JFrame f = new MyFrame1();
+			JFrame f = new MyFrame1(d);
 			f.pack();
 			f.show();
 
 			
 		}
+		else if(answer == 1){
+			JFrame p = new MyFrame2(d);
+			p.pack();
+			p.show();
+			//JOptionPane.showMessageDialog(null, answer);
+		}
+		
 		else {
-			JOptionPane.showMessageDialog(null, answer);
+			JFrame q = new MyFrame3(d);
+			q.pack();
+			q.show();
 		}
-		
-		/*JTextField Id = new JTextField(10);
-		JLabel nome = new JLabel("Nome: ");
-		nome.setLabelFor(Id);
-		nome.setDisplayedMnemonic('n');
-		
-		JTextField aut = new JTextField(10);
-		JLabel senha = new JLabel("Senha: ");
-		senha.setLabelFor(aut);
-		senha.setDisplayedMnemonic('d');
-		
-		
-		JButton botao = new JButton("Entrar");
-		botao.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		}
-		
-		//this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setDefaultCloseOperation(operation);
-		this.getContentPane().setLayout(new FlowLayout());
-		this.getContentPane().add(nome);
-		this.getContentPane().add(Id);
-		this.getContentPane().add(senha);
-		this.getContentPane().add(aut);
-		
-		
-		this.getContentPane().add(botao);
-		
-		
-		public boolean checaNome(String nome) {
-			if(d.getAlunos())
-		}*/
-		
-		
 		
 	}
 	
