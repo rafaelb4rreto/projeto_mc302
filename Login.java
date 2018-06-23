64c8afc9747b2f69808bdc950f5695a631878d80
jpsoubihe <jp.soubihe@gmail.com>
2018-06-23 13:02:56 -0300

    abre a tela para nome e senha e os armazena

--- Login.java
+++ Login.java
@@ -12,8 +12,21 @@ public class Login extends JFrame {
 	public Login() {
 		
 		Object[] classes = {"Aluno","Professor","Diretor"};
-		int answer = JOptionPane.showOptionDialog(null,"Que área gostaria de fazer login?","Tela de inicio", JOptionPane.INFORMATION_MESSAGE, JOptionPane.INFORMATION_MESSAGE,null, classes, classes[2]);
-		JOptionPane.showMessageDialog(null, answer);
+		int answer = JOptionPane.showOptionDialog(null,"Que Ã¡rea gostaria de fazer login?","Tela de inicio", JOptionPane.INFORMATION_MESSAGE, JOptionPane.INFORMATION_MESSAGE,null, classes, classes[2]);
+		
+		
+		if(answer == 0) { //Aluno esta tentando logar no sistema
+			//String ra = JOptionPane.showInputDialog(null, "RA : ", "Cadastro", JOptionPane.PLAIN_MESSAGE);
+			
+			JFrame f = new MyFrame1();
+			f.pack();
+			f.show();
+
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(null, answer);
+		}
 		
 		/*JTextField Id = new JTextField(10);
 		JLabel nome = new JLabel("Nome: ");
