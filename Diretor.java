e587658ea152ef2534249fc864bac4e06681966a
gubraz <38592740+gubraz@users.noreply.github.com>
2018-06-23 20:43:40 -0300

    Salvar e Carregar implementados

--- Diretor.java
+++ Diretor.java
@@ -1,8 +1,6 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Formatter;
-import java.util.Scanner;
 
 public class Diretor extends Pessoa{
 	
--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import javax.swing.*;
 
 
