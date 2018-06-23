cd67dad86d7e53a6731e2cd2891b3272a9b39117
jpsoubihe <jp.soubihe@gmail.com>
2018-06-23 13:03:28 -0300

    auxilia login()

--- MyFrame1.java
+++ MyFrame1.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.event.*;
 
 public class MyFrame1 extends JFrame{
 	/**
@@ -8,12 +10,42 @@ public class MyFrame1 extends JFrame{
 
 	public MyFrame1() {
 		
-	super("Escola X");
-	final JLabel label = new JLabel("Bem vindo!");
-	this.getContentPane().add(label);
-	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	this.pack();
-	this.setVisible(true);
+	super("Cadastro");
+	JTextField Id = new JTextField(10);
+	JLabel nome = new JLabel("Nome: ");
+	nome.setLabelFor(Id);
+	nome.setDisplayedMnemonic('n');
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	this.getContentPane().setLayout(new FlowLayout());
+	this.getContentPane().add(nome);
+	this.getContentPane().add(Id);
+	
+	JTextField aut = new JTextField(10);
+	JLabel senha = new JLabel("Senha: ");
+	senha.setLabelFor(aut);
+	senha.setDisplayedMnemonic('d');
+	this.getContentPane().add(senha);
+	this.getContentPane().add(aut);
+	
+	String nomeValue = Id.getText();
+	JButton botao = new JButton("Entrar");
+	this.getContentPane().add(botao);
+
+	
+	botao.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JOptionPane.showMessageDialog(null, Id.getText());			
+		}
+	});
+	System.out.println("Conteudo :" +nomeValue);
+	
+	
+	
+	
+	
+	
 	}
 
 	
